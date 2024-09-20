a22c1a55e32c8e5ee12b2072da42c6bf1a50b7d5
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-09-20 11:41:50 -0400

    Added class with information about Arti Lolja

---
+++ ICS3U-LoljaA/src/unit1/AboutMe.java
@@ -0,0 +1,18 @@
+package unit1;
+/**
+ * Description: This program prints information about the author, Arti Lolja.
+ * Date: Sept 20, 2024
+ * Author: @author Arti Lolja
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This prints out information about Arti
+		System.out.println("Hello, my name is Arti Lolja.");	
+		System.out.println("Right now, I am in grade 11.");
+		System.out.println("My teacher for the ICS3U course is Ms. Kemp.");
+		System.out.println("The school I go to is Lawrence Park Collegiate Institute.");
+
+	}
+
+}
