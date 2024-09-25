311a18eca7080278cc7fd310909b9bc85da2bcf2
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-09-25 10:56:17 -0400

    beginning to revamp about me and finished bingo assignment

--- ICS3U-LoljaA/src/unit1/AboutMe.java
+++ ICS3U-LoljaA/src/unit1/AboutMe.java
@@ -17,40 +17,31 @@ public class AboutMe {
 		// These next statements display information about my timetable on Day 1 LRegular schedule
 		System.out.println("Week of September 23-27 Timetable");
 		System.out.println("---------------------------------");
-		System.out.format("Day 1 (Monday, Friday): \t%-7sICS3U- %7s", "Period 1:", "09:00-10:15");
-		System.out.println("");
-		System.out.format("\t%33sBAF3M- %-12s", "Period 2:", "10:20-11:40");
-		System.out.println("");
-		System.out.format("\t%37s11:40-12:40", "Lunch Break-\t");
-		System.out.println("");
-		System.out.format("\t%33sMCR3U- %-12s", "Period 3:", "12:40-13:55");
-		System.out.println("");
-		System.out.format("\t%33sSCH3U- %-11s","Period 4:", "14:00-15:15");
-		System.out.println("");
-		System.out.println("--------------------------------------------------------------");
+		System.out.format("\nDay 1 (Monday, Friday): \t%-7sICS3U- %7s", "Period 1:", "09:00-10:15");
+		System.out.format("\n\t%33sBAF3M- %-12s", "Period 2:", "10:20-11:40");
+		System.out.format("\n\t%37s11:40-12:40", "Lunch Break-\t");
+		System.out.format("\n\t%33sMCR3U- %-12s", "Period 3:", "12:40-13:55");
+		System.out.format("\n\t%33sSCH3U- %-11s","Period 4:", "14:00-15:15");
+		System.out.println("\n--------------------------------------------------------------");
 		// These next statements display information about my timetable on Day 1 Late start schedule
-		System.out.format("Day 1 (Wednesday Late Start): %11sICS3U- %7s", "Period 1:", "09:55-10:55");
-		System.out.println("");
-		System.out.format("%41sBAF3M- %10s", "Period 2:", "11:00-12:05");
-		System.out.println("");
-		System.out.format("\t%37s12:05-13:05", "Lunch Break-\t");
-		System.out.println("");
-		System.out.format("%41sMCR3U- %10s", "Period 3:", "13:05-14:10");
-		System.out.println("");
-		System.out.format("%41sSCH3U- %10s", "Period 4:", "14:15-15:15");
-		System.out.println("");
-		System.out.println("--------------------------------------------------------------");
+		System.out.format("\nDay 1 (Wednesday Late Start): %11sICS3U- %7s", "Period 1:", "09:55-10:55");
+		System.out.format("\n%41sBAF3M- %10s", "Period 2:", "11:00-12:05");
+		System.out.format("\n\t%37s12:05-13:05", "Lunch Break-\t");
+		System.out.format("\n%41sMCR3U- %10s", "Period 3:", "13:05-14:10");
+		System.out.format("\n%41sSCH3U- %10s", "Period 4:", "14:15-15:15");
+		System.out.println("\n--------------------------------------------------------------");
 		// These next statements display information about my timetable on Day 2 Regular Schedule
-		System.out.format("Day 2 (Tuesday, Thursday): %14sBAF3M- %7s", "Period 1:", "09:00-10:15");
-		System.out.println("");
-		System.out.format("%41sICS3U- %10s", "Period 2:", "10:20-11:40");
-		System.out.println("");
-		System.out.format("\t%37s11:40-12:40", "Lunch Break-\t");
-		System.out.println("");
-		System.out.format("%41sSCH3U- %10s", "Period 3:", "12:40-13:55");
-		System.out.println("");
-		System.out.format("%41sMCR3U- %10s", "Period 4:", "14:00-15:15");
-		
+		System.out.format("\nDay 2 (Tuesday, Thursday): %14sBAF3M- %7s", "Period 1:", "09:00-10:15");
+		System.out.format("\n%41sICS3U- %10s", "Period 2:", "10:20-11:40");
+		System.out.format("\n\t%37s11:40-12:40", "Lunch Break-\t");
+		System.out.format("\n%41sSCH3U- %10s", "Period 3:", "12:40-13:55");
+		System.out.format("\n%41sMCR3U- %10s", "Period 4:", "14:00-15:15");
+		// CHART VERSION OF TIMETABLE\
+		System.out.println("\n_________________________________________________________________________________________________________________________________");
+		System.out.format("%-25s|%-25s|%-25s|%-25s|%-25s|", "|Monday (23rd)", "Tuesday (24th)", "Wednesday (25th)", "Thursday (26th)", "Friday (27th)");
+		System.out.println("\n|________________________|_________________________|_________________________|_________________________|_________________________|");
+		System.out.format("%-25s|%-25s|%-25s|%-25s|%-25s|", "|ICS3U-1", "BAF3M-3", "ICS3U-1", "BAF3M-3", "ICS3U-1");
+		System.out.format("\n%-25s|%-25s|%-25s|%-25s|%-25s|", "|9:00-10:15", "9:00-10:15", "9:55-10:55", "9:00-10:15", "9:00-10:15");
 
 	}
 
---
+++ ICS3U-LoljaA/src/unit1/Bingo.java
@@ -0,0 +1,41 @@
+package unit1;
+
+/**
+ * 
+ */
+public class Bingo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// This print out rules for playing bingo
+		System.out.println("These are the rules on how to play Bingo!");
+		System.out.println("________________________________________");
+		System.out.println("\n1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println("\n2. The number is placed on the bingo board and called out.");
+		System.out.println("\n3. Players look for the called number on their bingo card.");
+		System.out.println("\n4. If the number is located, it is marked off.");
+		System.out.println("\n5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.");
+		System.out.println("\n6. The winning player yells \"BINGO\"");
+		System.out.println("\n________________________________________");
+		// These lines of code print out the bingo card
+		System.out.println("\nARTI's BINGO CARD");
+		System.out.println("________________________________________");
+		System.out.format("\n%10s%10s%10s%10s%10s", "B\t|", "I\t|", "N\t|", "G\t|", "O\t|");
+		System.out.format("\n_________________________________________________________________________________");
+		System.out.format("\n%10s%10s%10s%10s%10s", "3\t|", "18\t|", "33\t|", "47\t|", "73\t|");
+		System.out.format("\n_________________________________________________________________________________");
+		System.out.format("\n%10s%10s%10s%10s%10s", "6\t|", "21\t|", "37\t|", "55\t|", "74\t|");
+		System.out.format("\n_________________________________________________________________________________");
+		System.out.format("\n%10s%10s%11s%10s%10s", "2\t|", "26\t|", "FREE\t|", "58\t|", "63\t|");
+		System.out.format("\n_________________________________________________________________________________");
+		System.out.format("\n%10s%10s%10s%10s%10s", "1\t|", "29\t|", "44\t|", "49\t|", "68\t|");
+		System.out.format("\n_________________________________________________________________________________");
+		System.out.format("\n%10s%10s%10s%10s%10s", "9\t|", "16\t|", "36\t|", "46\t|", "66\t|");
+		
+		
+
+	}
+
+}
