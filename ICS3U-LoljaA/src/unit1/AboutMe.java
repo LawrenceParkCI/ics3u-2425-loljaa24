2b43d1afef21f4b619e2adaa5da28bd862b9808d
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-09-26 11:34:29 -0400

    finished revamp of about me and bingo, did ascii art program

--- ICS3U-LoljaA/src/unit1/AboutMe.java
+++ ICS3U-LoljaA/src/unit1/AboutMe.java
@@ -13,36 +13,29 @@ public class AboutMe {
 		System.out.println("My teacher for the ICS3U course is Ms. Kemp.");
 		System.out.println("The school I go to is Lawrence Park Collegiate Institute.");
 		System.out.println("\"Go Panthers!\"");
-		System.out.println("---------------------");
-		// These next statements display information about my timetable on Day 1 LRegular schedule
+		System.out.println("---------------------------------");
+		// These next statements display my timetable on the week of September 23-27
 		System.out.println("Week of September 23-27 Timetable");
 		System.out.println("---------------------------------");
-		System.out.format("\nDay 1 (Monday, Friday): \t%-7sICS3U- %7s", "Period 1:", "09:00-10:15");
-		System.out.format("\n\t%33sBAF3M- %-12s", "Period 2:", "10:20-11:40");
-		System.out.format("\n\t%37s11:40-12:40", "Lunch Break-\t");
-		System.out.format("\n\t%33sMCR3U- %-12s", "Period 3:", "12:40-13:55");
-		System.out.format("\n\t%33sSCH3U- %-11s","Period 4:", "14:00-15:15");
-		System.out.println("\n--------------------------------------------------------------");
-		// These next statements display information about my timetable on Day 1 Late start schedule
-		System.out.format("\nDay 1 (Wednesday Late Start): %11sICS3U- %7s", "Period 1:", "09:55-10:55");
-		System.out.format("\n%41sBAF3M- %10s", "Period 2:", "11:00-12:05");
-		System.out.format("\n\t%37s12:05-13:05", "Lunch Break-\t");
-		System.out.format("\n%41sMCR3U- %10s", "Period 3:", "13:05-14:10");
-		System.out.format("\n%41sSCH3U- %10s", "Period 4:", "14:15-15:15");
-		System.out.println("\n--------------------------------------------------------------");
-		// These next statements display information about my timetable on Day 2 Regular Schedule
-		System.out.format("\nDay 2 (Tuesday, Thursday): %14sBAF3M- %7s", "Period 1:", "09:00-10:15");
-		System.out.format("\n%41sICS3U- %10s", "Period 2:", "10:20-11:40");
-		System.out.format("\n\t%37s11:40-12:40", "Lunch Break-\t");
-		System.out.format("\n%41sSCH3U- %10s", "Period 3:", "12:40-13:55");
-		System.out.format("\n%41sMCR3U- %10s", "Period 4:", "14:00-15:15");
 		// CHART VERSION OF TIMETABLE\
-		System.out.println("\n_________________________________________________________________________________________________________________________________");
-		System.out.format("%-25s|%-25s|%-25s|%-25s|%-25s|", "|Monday (23rd)", "Tuesday (24th)", "Wednesday (25th)", "Thursday (26th)", "Friday (27th)");
-		System.out.println("\n|________________________|_________________________|_________________________|_________________________|_________________________|");
-		System.out.format("%-25s|%-25s|%-25s|%-25s|%-25s|", "|ICS3U-1", "BAF3M-3", "ICS3U-1", "BAF3M-3", "ICS3U-1");
-		System.out.format("\n%-25s|%-25s|%-25s|%-25s|%-25s|", "|9:00-10:15", "9:00-10:15", "9:55-10:55", "9:00-10:15", "9:00-10:15");
-
+		System.out.println("\n____________________________________________________________________________________________________________________________________________");
+		System.out.format("%-11s%-25s|%-25s|%-25s|%-25s|%-25s|", "|Periods", "|Monday (23rd)", "Tuesday (24th)", "Wednesday (25th)", "Thursday (26th)", "Friday (27th)");
+		System.out.println("\n|__________|________________________|_________________________|_________________________|_________________________|_________________________|");
+		System.out.format("%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|Period 1", "ICS3U-1", "BAF3M-3", "ICS3U-1", "BAF3M-3", "ICS3U-1");
+		System.out.format("\n%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|", "09:00-10:15", "09:00-10:15", "09:55-10:55", "09:00-10:15", "09:00-10:15");
+		System.out.println("\n|__________|________________________|_________________________|_________________________|_________________________|_________________________|");
+		System.out.format("%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|Period 2", "BAF3M-3", "ICS3U-1", "BAF3M-3", "ICS3U-1", "BAF3M-3");
+		System.out.format("\n%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|", "10:20-11:40", "10:20-11:40", "11:00-12:05", "10:20-11:40", "10:20-11:40");
+		System.out.println("\n|__________|________________________|_________________________|_________________________|_________________________|_________________________|");
+		System.out.format("%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|Lunch Hour", "Lunch Break", "Lunch Break", "Lunch Break", "Lunch Break", "Lunch Break");
+		System.out.format("\n%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|", "11:40-12:40", "11:40-12:40", "12:05-13:05", "11:40-12:40", "11:40-12:40");
+		System.out.println("\n|__________|________________________|_________________________|_________________________|_________________________|_________________________|");
+		System.out.format("%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|Period 3", "MCR3U1-2", "SCH3U1-4", "MCR3U1-2", "SCH3U1-4", "MCR3U1-2");
+		System.out.format("\n%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|", "12:40-13:55", "12:40-13:55", "13:05-14:10", "12:40-13:55", "12:40-13:55");
+		System.out.println("\n|__________|________________________|_________________________|_________________________|_________________________|_________________________|");
+		System.out.format("%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|Period 4", "SCH3U1-4", "MCR3U1-2", "SCH3U1-4", "MCR3U1-2", "SCH3U1-4");
+		System.out.format("\n%-11s|%-24s|%-25s|%-25s|%-25s|%-25s|", "|", "14:00-15:15", "14:00-15:15", "14:15-15:15", "14:00-15:15", "14:00-15:15");
+		System.out.println("\n|__________|________________________|_________________________|_________________________|_________________________|_________________________|");
 	}
 
 }
---
+++ ICS3U-LoljaA/src/unit1/AsciiArt.java
@@ -0,0 +1,35 @@
+package unit1;
+	/**
+	 * Description: This program states my ASCII ART signatures
+	 * Date: Sept 26, 2024
+	 * Author: @author Arti Lolja
+	 */
+
+public class AsciiArt {
+
+	public static void main(String[] args) {
+		// These lines of codes state out my first ASCII ART signature
+	    System.out.println("                                                                 ");
+	    System.out.println("  ,---.           ,--.  ,--.    ,--.          ,--.  ,--.         ");
+	    System.out.println(" /  O  \\ ,--.--.,-'  '-.`--'    |  |    ,---. |  |  `--' ,--,--. ");
+	    System.out.println("|  .-.  ||  .--''-.  .-',--.    |  |   | .-. ||  |  ,--.' ,-.  | ");
+	    System.out.println("|  | |  ||  |     |  |  |  |    |  '--.' '-' '|  |  |  |\\ '-'  | ");
+	    System.out.println("`--' `--'`--'     `--'  `--'    `-----' `---' `--'.-'  / `--`--' ");
+	    System.out.println("                                                  '---'          ");
+	    // These lines of codes state out my second ASCII ART image
+	    System.out.println("   __________   ");
+	    System.out.println("  / ___  ___ \\  ");
+	    System.out.println(" / / @ \\/ @ \\ \\");
+	    System.out.println(" \\ \\___/\\___/ /\\");
+	    System.out.println("  \\____\\/____/||");
+	    System.out.println("  /     /\\\\\\\\\\//");
+	    System.out.println("  |     |\\\\\\\\\\\\");
+	    System.out.println("   \\     \\\\\\\\\\\\");
+	    System.out.println("    \\_____/\\\\\\\\");
+	    System.out.println("     _||_||_");
+	    System.out.println("      -- --");
+	    
+	                                                                                                      
+	}
+
+}
