aa503336e8b7438c8fa72c774cd8419191867adf
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-10-02 10:54:19 -0400

    save program makelabels again for step 4

--- ICS3U-LoljaA/src/unit1/MakeLabels.java
+++ ICS3U-LoljaA/src/unit1/MakeLabels.java
@@ -1,15 +1,27 @@
 package unit1;
-
 import java.util.Scanner;
-
+/**
+ * Description: This program teaches the programmer about scanner
+ * Date: Oct 2, 2024
+ * @author Arti Lolja
+ */
 public class MakeLabels {
 
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		System.out.println();
+		
+		String subject;
+		
+		System.out.println("Type in the subject and press <Enter>");
+		subject = sc.next();
+		
 		System.out.println("******************************");
-		System.out.println("John");
-		System.out.println("Computers");
+		System.out.println("Tenzin");
+		System.out.println(subject);
 		System.out.println("******************************");
 		sc.close();
 
