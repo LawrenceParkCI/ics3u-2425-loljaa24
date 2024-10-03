a8b45c13e5357dc56b0677f860f53dea23d96aab
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-10-03 09:38:15 -0400

    finished inputfp program

--- ICS3U-LoljaA/src/unit1/InputFP.java
+++ ICS3U-LoljaA/src/unit1/InputFP.java
@@ -14,16 +14,20 @@ public class InputFP {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		// declare variables
 		double num1, num2;
-
+		
+		// receive user input
 		System.out.println("Type in one floating point number");
 		num1 = sc.nextDouble();
 		System.out.println("Type in another floating point number");
 		num2 = sc.nextDouble();
-
+		
+		// print the output
 		System.out.println();
 		System.out.println("The first number entered was " + num1);
 		System.out.println("The second number entered was " + num2);
+		sc.close();
 
 
 	}
