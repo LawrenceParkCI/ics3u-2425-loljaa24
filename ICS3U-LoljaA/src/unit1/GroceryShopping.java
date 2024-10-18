0efed56d7fe2ee4165c71bf093f020a522694c10
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-10-18 11:40:30 -0400

    started groceryshopping assignment

---
+++ ICS3U-LoljaA/src/unit1/GroceryShopping.java
@@ -0,0 +1,21 @@
+package unit1;
+
+import java.util.Scanner;
+
+/**
+ * Description:
+ * Date: Oct 18, 2024
+ * @author Arti Lolja
+ */
+public class GroceryShopping {
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("| | $$$ || \"Mr. Lolja's Groceries\" || $$$ | |");
+
+	}
+
+}
