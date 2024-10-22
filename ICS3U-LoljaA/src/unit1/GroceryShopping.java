69e4f6fc181476c112d22eed104ee113235b8e90
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-10-22 11:39:07 -0400

    finished groceryshopping program

--- ICS3U-LoljaA/src/unit1/GroceryShopping.java
+++ ICS3U-LoljaA/src/unit1/GroceryShopping.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
- * Description:
+ * Description: This program prints out a grocery receipt
  * Date: Oct 18, 2024
  * @author Arti Lolja
  */
@@ -26,29 +26,29 @@ public class GroceryShopping {
 	// receive user input for first grocery item
 	System.out.println("| | $$$ || \"Mr. Lolja's Groceries\" || $$$ | |");
 	System.out.println("_____________________________________________");
-	System.out.println("\nWhat product are you purchasing?");
+	System.out.print("\nWhat product are you purchasing: ");
 	item1 = UPC.next();
-	System.out.println("How much does it cost?");
+	System.out.print("How much does it cost: ");
 	cost1 = UPC.nextDouble();
-	System.out.println("How much of this item are you buying?");
+	System.out.print("How much of this item are you buying: ");
 	amount1 = UPC.nextInt();
 	System.out.println("Awesome!");
 	
 	// receive user input for second grocery
-	System.out.println("\nWhat other product are you purchasing?");
+	System.out.print("\nWhat other product are you purchasing: ");
 	item2 = UPC.next();
-	System.out.println("How much does it cost?");
+	System.out.print("How much does it cost: ");
 	cost2 = UPC.nextDouble();
-	System.out.println("How much of this item are you buying?");
+	System.out.print("How much of this item are you buying: ");
 	amount2 = UPC.nextInt();
 	System.out.println("Good!");
 	
 	// begin print output
 	System.out.println("\nThis is your receipt:\n\n");
-	System.out.format("%25s", "Mr. Lolja's Grocery");
+	System.out.format("%49s", "Mr. Lolja's Grocery");
 
 	// formats
-	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("\nyyyy/MM/dd HH:mm:ss");  
 	LocalDateTime now = LocalDateTime.now();  
 	System.out.println(dtf.format(now));
 	NumberFormat money = NumberFormat.getCurrencyInstance();
@@ -60,14 +60,21 @@ public class GroceryShopping {
 	double subtotal = itemtotalcost1 + itemtotalcost2;
 	double taxtotal = subtotal * taxrate;
 	double finaltotal = subtotal + taxtotal;
+	int finaltotal2 = (int)finaltotal;
 	
-	System.out.format("%-10s| %-10s| %-10s| %-10s|", "|Item\t", "Price", "Quant.", "Total Price ");
-	System.out.println("\n|___________|___________|___________|_____________|");
-	System.out.format("|%-10s | %-10s| %-10s| %-10s  |", item1, money.format(cost1), amount1, money.format(itemtotalcost1));
+	System.out.format("\n%-15s| %-15s| %-15s| %-15s|", "| Item\t", "Price", "Quant.", "Total Price   ");
+	System.out.println("\n|_______________|________________|________________|________________|");
+	System.out.format("|%-15s| %15s| %15s| %15s|", item1, money.format(cost1), amount1, money.format(itemtotalcost1));
+	System.out.format("\n|%-15s| %15s| %15s| %15s|", item2, money.format(cost2), amount2, money.format(itemtotalcost2));
+	System.out.println("\n|_______________|________________|________________|________________|");
+	System.out.format("%50s %16s", "Subtotal:", money.format(subtotal));
+	System.out.format("\n%50s %16s", "Tax:", money.format(taxtotal));
+	System.out.format("\n%50s %16s", "Total:", money.format(finaltotal));
+	System.out.println("\n\nThis is approximately $" + finaltotal2 +".");
+	System.out.println("\nHave a good rest of your day!");
+	System.out.println("Thank you for visiting Mr.Lolja's grocery!");
 	
-	
-	
-	
+	UPC.close();
 	}
 
 }
