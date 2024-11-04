25ae25a2fb3f046f0f9cd370aa8f73733a81b8b6
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-11-04 10:57:34 -0500

    finished oddeven, positivenegative programs

--- ICS3U-LoljaA/src/unit2/OddEven.java
+++ ICS3U-LoljaA/src/unit2/OddEven.java
@@ -1,10 +1,34 @@
 package unit2;
 
+import java.util.Scanner;
+/**
+ * Description: This program determines whether an integer is even or odd
+ * Date: Nov 4, 2024
+ * @author Arti Lolja
+ */
 public class OddEven {
-
+	/**
+	 * This is the entry point to the program
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		//declare variables
+		int num;
+		
+		//input an integer
+		System.out.print("Type in an integer: ");
+		num = sc.nextInt();
+		
+		//print output
+		if (num % 2 == 0) {
+			System.out.println("The number is even.");
+		}
+		else {
+			System.out.println("The number provided is odd.");
+		}
+		sc.close();
 	}
 
 }
--- ICS3U-LoljaA/src/unit2/PositiveNegative.java
+++ ICS3U-LoljaA/src/unit2/PositiveNegative.java
@@ -1,10 +1,42 @@
 package unit2;
 
+import java.util.Scanner;
+/**
+ * Description: This program determines whether an integer is positive/negative and if it's divisible by 7
+ * Date: Nov 4, 2024
+ * @author Arti Lolja
+ */
 public class PositiveNegative {
-
+	/**
+	 * This is the entry point tothe program
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		//declare variable
+		int num;
+		
+		//receive input
+		System.out.println("Type in an integer");
+		num = sc.nextInt();
+		
+		//print 1st output
+		if (num >= 0) {
+			System.out.println("This integer is positiv.e");
+		}
+		else {
+			System.out.println("This intege is negative.");
+		}
+		// print 2nd output
+		if (num % 7 == 0) {
+			System.out.println("This integer is divisble by 7.");
+		}
+		else {
+			System.out.println("This integer is not divisble by 7.");
+		}
+		sc.close();
+		
 	}
 
 }
