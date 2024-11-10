51bd73d247b2553e227fa735210b54cd7851964a
slolj <slolj@LAPTOP-DQTCAKNQ.phub.net.cable.rogers.com>
2024-11-10 11:24:10 -0500

    Final fixes to GroceryShopping program

--- ICS3U-LoljaA/src/unit1/GroceryShopping.java
+++ ICS3U-LoljaA/src/unit1/GroceryShopping.java
@@ -23,7 +23,7 @@ public class GroceryShopping {
 	int amount1, amount2;
 	
 	// Receiving user input for the first grocery item:
-	System.out.println("| | $$$ || \"Mr. Lolja's Groceries\" || $$$ | |");
+	System.out.println("/ / $$$ //  Mr. Lolja's Groceries  \\\\ $$$ \\ \\");
 	System.out.println("_____________________________________________");
 	System.out.print("\nWhat product are you purchasing? : ");
 	item1 = UPC.next();
@@ -45,6 +45,7 @@ public class GroceryShopping {
 	// Starting to print out output:
 	System.out.println("\nThis is your receipt:\n\n");
 	System.out.format("%49s", "Mr. Lolja's Grocery");
+	System.out.format("\n%49s", "___________________");
 
 	// Formats:
 	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("\nyyyy/MM/dd HH:mm:ss");  
@@ -62,7 +63,8 @@ public class GroceryShopping {
 	double nearestfive = Math.round(finaltotal/0.05)*0.05;
 	
 	// Receipt:
-	System.out.format("\n%-15s| %-15s| %-15s| %-15s|", "| Item\t", "Price", "Quantity", "Total Price   ");
+	System.out.println("____________________________________________________________________");
+	System.out.format("%-15s| %-15s| %-15s| %-15s|", "| Item\t", "Price", "Quantity", "Total Price   ");
 	System.out.println("\n|_______________|________________|________________|________________|");
 	System.out.format("|%-15s| %15s| %15s| %15s|", item1, money.format(cost1), amount1, money.format(itemtotalcost1));
 	System.out.format("\n|%-15s| %15s| %15s| %15s|", item2, money.format(cost2), amount2, money.format(itemtotalcost2));
@@ -72,10 +74,11 @@ public class GroceryShopping {
 	System.out.format("\n%50s %16s", "Total:", money.format(finaltotal));
 	
 	// End of output; saying goodbye to the customer.
-	System.out.println("\n\nThis is approximately $" + finaltotal2 +".");
-	System.out.println("If you are paying with cash, that will be " + money.format(nearestfive));
+	System.out.println("\n\nThis is approximately $" + finaltotal2 + ".");
+	System.out.println("If you are paying with cash, that will be " + money.format(nearestfive) + ".");
 	System.out.println("\nHave a good rest of your day!");
 	System.out.println("Thank you for visiting Mr.Lolja's grocery!");
+	System.out.println("\n/ / $$$ //  Mr. Lolja's Groceries  \\\\ $$$ \\ \\");
 	
 	UPC.close();
 	}
