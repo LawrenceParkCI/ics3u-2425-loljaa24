ff17915f5bfcc5daaca3d143c944114cc37fcd72
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-11-14 11:35:29 -0500

    improved IfChallenge3

--- ICS3U-LoljaA/src/unit2/IfChallenge3.java
+++ ICS3U-LoljaA/src/unit2/IfChallenge3.java
@@ -9,8 +9,9 @@ public class IfChallenge3 {
 	/**
 	 * This is the entry point to the program
 	 * @param args unused
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		//This is for the portfolio
 
 		/*
@@ -36,7 +37,7 @@ public class IfChallenge3 {
 		Scanner sc = new Scanner(System.in);
 		// Declaring variables:
 		String s1, s2;
-		String a1, a2;
+		String a1, a2, a4, a5, a6;
 		int a3;
 		double quizPoints = 0;
 		// Receiving name and opinion before starting quiz...
@@ -51,8 +52,8 @@ public class IfChallenge3 {
 			System.out.println("\nToo bad! The quiz is going to start now!"); }
 
 		// FIRST QUESTION!!!
-		System.out.println("\n The first question is...");
-		System.out.println("\n What is the capital of Australia?");
+		System.out.println("\nThe first question is...");
+		System.out.println("\nWhat is the capital of Australia?");
 		System.out.println("A) Sydney");
 		System.out.println("B) Melbourne");
 		System.out.println("C) Canberra");
@@ -66,8 +67,8 @@ public class IfChallenge3 {
 			System.out.println("\nYou got fooled there, you do not gain a mark for that."); }
 			
 		// SECOND QUESTION!!!
-		System.out.println("\n The second question is...");
-		System.out.println("\n The capital of Brazil is Rio de Janeiro");
+		System.out.println("\nThe second question is...");
+		System.out.println("\nThe capital of Brazil is Rio de Janeiro");
 		System.out.println("A) TRUE");
 		System.out.println("B) FALSE");
 		System.out.print("Type in your answer here: ");
@@ -79,7 +80,7 @@ public class IfChallenge3 {
 			System.out.println("\nHow did you not get that?? You are not getting a point"); }
 		
 		// THIRD QUESTION!!!
-		System.out.println("\n The third question is...");
+		System.out.println("\nThe third question is...");
 		System.out.println("\nHow many capital cities does the Republic of South Africa have?");
 		System.out.print("Type in your answer here (an integer): ");
 		a3 = sc.nextInt();
@@ -90,10 +91,74 @@ public class IfChallenge3 {
 			System.out.println("\nThat's okay. You get a point for effort. They have three capital cities.");
 			quizPoints += 1; }
 		
+		// FOURTH QUESTION!!
+		System.out.println("\nThe fourth question is...");
+		System.out.println("\nWhat is the capital city of the canadian province that does not have provincial sales tax?");
+		System.out.println("GENERATING...");
+		Thread.sleep(1000);
+		System.out.println("\n            .:..                 \r\n"
+				+ "             %..=*-.              \r\n"
+				+ "          ..%.     .=#=....       \r\n"
+				+ "          .*..        ..:+*-....  \r\n"
+				+ "         .+.              ....**-.\r\n"
+				+ "        .+..                  .--.\r\n"
+				+ "       .+:                  . .*. \r\n"
+				+ "      .+:.                    *:. \r\n"
+				+ "     .+-.                  ...=.  \r\n"
+				+ "    .=-.                    .%.   \r\n"
+				+ "   .-:.                    .-..   \r\n"
+				+ "  .-:.                   ...#..   \r\n"
+				+ " ..*.                     .*...   \r\n"
+				+ " .-+.                    .-=...   \r\n"
+				+ "  .-+.                   .*..     \r\n"
+				+ "   .%.                  .+:       \r\n"
+				+ "   .:=.                ..+.       \r\n"
+				+ "    .#.                .*.        \r\n"
+				+ "     .#.              .-=         \r\n"
+				+ "     .-+.             .*.         \r\n"
+				+ "      .*.            .*:          \r\n"
+				+ "      ..*.          ..=.          \r\n"
+				+ "        :=          .%..          \r\n"
+				+ "       .*.         .::.           \r\n"
+				+ "       :=.        ..%.            \r\n"
+				+ "       .=*-..     .*.             \r\n"
+				+ "           .:#*:. -=.             \r\n"
+				+ "               ..:-               ");
+		System.out.println("\nA) Vancouver");
+		System.out.println("B) Regina");
+		System.out.println("C) Calgary");
+		System.out.println("D) Edmonton");
+		System.out.println("E) None of the Above");
+		System.out.print("Type your answer here: ");
+		a4 = sc.next();
+		if (a4.equalsIgnoreCase("D)") || a4.equalsIgnoreCase("D") || a4.equalsIgnoreCase("Edmonton")) {
+			System.out.println("\nThat is correct! You gain a point");
+			quizPoints += 1; }
+		else {
+			System.out.println("\nWomp womp. No points.");	}
+		
+		// FIFTH QUESTION
+		System.out.println("\nThe fifth question is...");
+		System.out.println("\nWhat are the capitals of the states in the USA that have the word \"Dakota\"");
+		System.out.println("A) Bismarck");
+		System.out.println("B) Pierre");
+		System.out.println("C) Cheyenne");
+		System.out.println("D) Helena");
+		System.out.print("Type the two answers here VVV");
+		System.out.print("\nYour first answer is: ");
+		a5 = sc.next();
+		System.out.print("Your second answer is: ");
+		a6 = sc.next();
+		if ((a5.equalsIgnoreCase("A)") || a5.equalsIgnoreCase("A") || a5.equalsIgnoreCase("Bismarck")) && (a6.equalsIgnoreCase("B)")) || a6.equalsIgnoreCase("B") || a6.equalsIgnoreCase("Pierre")) {
+			System.out.println("\nWOW! You got it correct! You get two points.");
+			quizPoints +=2; }
+		else {
+			System.out.println("\nEven if you got atleast 1 right, you still get zero points!"); }
+		
 		//CONCLUSION
 		System.out.println("\nCongratulations on completing the quiz!");
-		System.out.println("Your final score is: " + quizPoints + "/4.0");
-		System.out.println("You got a grade of " + ((quizPoints / 4)*100) + "%.");
+		System.out.println("Your final score is: " + quizPoints + "/7.0");
+		System.out.println("You got a grade of " + (Math.rint((quizPoints / 7)*100)) + "%.");
 		if (quizPoints == 4) {
 			System.out.println("\nGood job!!! You got a perfect score."); }
 		else if (quizPoints == 3) {
