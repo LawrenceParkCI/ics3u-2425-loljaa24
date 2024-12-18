e22c3aa3467616abe7ec5ae7125c12bac578ba73
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2024-12-18 12:00:55 -0500

    Updated MathPlus

--- ICS3U-LoljaA/src/unit3/MathPlus.java
+++ ICS3U-LoljaA/src/unit3/MathPlus.java
@@ -10,10 +10,18 @@ public class MathPlus {
 	 * @param args unused
 	 */
 	public static void main(String[] args) {
-
+		double d1 = distance(5, 4, 6, 7);
+		System.out.println(d1);
+		double h = hypotenuse(10.0, 5.5);
+		System.out.println(h);
+		int f = numOfFactors(48);
+		System.out.println(f);
+		boolean p = isPrime(547);
+		System.out.println(p);
 	}
 
 	/**
+	 * Description: Finds out the distance between two points
 	 * @param x1 => point 1 on x axis
 	 * @param y1 => point 1 on y axis
 	 * @param x2 => point 2 on x axis
@@ -26,6 +34,7 @@ public class MathPlus {
 	}
 
 	/**
+	 * Description: Finds out the length of the hypotenuse of a right triangle.
 	 * @param a => value of side A
 	 * @param b => value of side B
 	 * @return => the value of the hypotenuse
@@ -36,27 +45,28 @@ public class MathPlus {
 	}
 
 	/**
+	 * Description: Finds how many factors a number has.
 	 * @param num1 => the value of the number
 	 * @return => value of counter
 	 */
-	public static int numOfFactors( int num1) {
-		int num2 = num1/2;
-		int counter = 0;
-		for (int i = 0; i < num2; i++) {
+	public static int numOfFactors( int num1 ) {
+		int counter = 1;
+		for (int i = 1; i < num1; i++) {
 			if ((num1 % i) == 0) {
-				counter += 2;
+				counter++;
 			}
 		}
 		return counter;
 	}
 
 	/**
+	 * Description: Determines whether a number is prime or not
 	 * @param num1 => value of the number
 	 * @return => whether the number is prime or not
 	 */
 	public static boolean isPrime( int num1) {
 		int num2 = num1/2;
-		for (int i= 1; i < num2; i++) {
+		for (int i= 2; i < num2; i++) {
 			if ((num1 % i) == 0) {
 				return false;
 			}
