94dfd8f5e3e0e7be5189cfd8e8a45a8c3450a615
342855897 <342855897@MXL1502T80.tdsb.on.ca>
2025-01-08 12:01:44 -0500

    Worked on my final project

--- ICS3U-LoljaA/src/unit3/Trigonometry.java
+++ ICS3U-LoljaA/src/unit3/Trigonometry.java
@@ -15,6 +15,8 @@ public class Trigonometry {
 		boolean restart = true; // Check to see if the user wants to repeat the program or not
 
 		// narrowing down the user's options through if, if else, and else statements
+		Math.asin(0.0956);
+		
 		do {
 			System.out.println("What trigonometric function do you want to use?");
 			System.out.println("1. SOH CAH TOA\n2. SINE LAW\n3. COSINE LAW\n(TYPE IN THE NUMBER CORRELATED WITH THE OPTION)");
@@ -38,10 +40,10 @@ public class Trigonometry {
 									System.out.println("\n1. OPPOSITE\n2. HYPOTENUSE\n(TYPE IN THE NUMBER CORRELATED WITH THE OPTION)");
 									choice = sc.next();
 									if (choice.equals("1") || choice.equals("1.")) {
-										
+
 									}
 									else if (choice.equals("2.") || choice.equals("2.")) {
-										
+
 									}
 									else {
 										System.out.println("UNKOWN RESPONSE");
@@ -49,7 +51,17 @@ public class Trigonometry {
 								} while (restart == true);
 							}
 							else if (choice.equals("2") || choice.equals("2.")) {
-								
+								System.out.println("\nOption \"ANGLE\" selected.");
+								System.out.println("State the opposite first and then the hypotenuse: (NUMBERS ONLY)");
+								int x = 2;
+								double[] sin = new double[x];
+								for (int i = 0; i < x; i++) {
+									sin[i] = sc.nextDouble();
+								}
+								double sum = SohAngle(sin);
+								System.out.println("Your two angles are " + sum + "° and " + (90-sum) + "°.");
+								System.out.println("Rounded, they are " + Math.rint(sum) + "° and " + Math.rint(sum) + "°.");
+								restart = false;
 							}
 							else {
 								System.out.println("UNKNOWN RESPONSE");
@@ -69,10 +81,10 @@ public class Trigonometry {
 									System.out.println("\n1. ADJACENT\n2. HYPOTENUSE\n(TYPE IN THE NUMBER CORRELATED WITH THE OPTION)");
 									choice = sc.next();
 									if (choice.equals("1") || choice.equals("1.")) {
-										
+
 									}
 									else if (choice.equals("2.") || choice.equals("2.")) {
-										
+
 									}
 									else {
 										System.out.println("UNKOWN RESPONSE");
@@ -80,7 +92,8 @@ public class Trigonometry {
 								} while (restart == true);
 							}
 							else if (choice.equals("2") || choice.equals("2.")) {
-
+								System.out.println("\nOption \"ANGLE\" selected.");
+								System.out.println("State first the adjacent and then the hypotenuse:");
 							}
 							else {
 								System.out.println("UNKNOWN RESPONSE");
@@ -100,10 +113,10 @@ public class Trigonometry {
 									System.out.println("\n1. OPPOSITE\n2. ADJACENT\n(TYPE IN THE NUMBER CORRELATED WITH THE OPTION)");
 									choice = sc.next();
 									if (choice.equals("1") || choice.equals("1.")) {
-										
+
 									}
 									else if (choice.equals("2.") || choice.equals("2.")) {
-										
+
 									}
 									else {
 										System.out.println("UNKOWN RESPONSE");
@@ -111,7 +124,8 @@ public class Trigonometry {
 								} while (restart == true);
 							}
 							else if (choice.equals("2") || choice.equals("2.")) {
-
+								System.out.println("\nOption \"ANGLE\" selected.");
+								System.out.println("State the opposite first and then the adjacent:");
 							}
 							else {
 								System.out.println("UNKNOWN RESPONSE");
@@ -133,5 +147,26 @@ public class Trigonometry {
 				System.out.println("UNKNOWN RESPONSE");
 			}
 		} while (restart == true);
+	}
+	
+	public static double RadtoDeg(double x) {
+		return x * (180/Math.PI);
+	}
+	/**
+	 * Description: This mnethod finds the way to solve for the two angles using SOH.
+	 * @param x => the two side lengths
+	 * @return => the first angle
+	 */
+	public static double SohAngle( double[] x ) {
+		if (x[1] == 0) {
+			return Double.NaN;
+		}
+		double y = (x[0] / x[1]);
+		if (y < 0 && y > 1) {
+			return Double.NaN;
+		}
+		else {
+			return RadtoDeg(Math.asin(y));
+		}
 	}
 }
